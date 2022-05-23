178cac7deb438b710ef5169346bc8e91832e6ae5
GabriPini <lele19977@gmail.com>
2022-05-23 12:36:47 +0200

    add snack-6

---
+++ Snack-6/index.php
@@ -0,0 +1,56 @@
+<!--  ## Snack 6
+Utilizzare questo array: https://pastebin.com/CkX3680A. Stampiamo il nostro array mettendo gli insegnanti in un rettangolo grigio e i PM in un rettangolo verde.
+ -->
+
+<?php
+
+$db = [
+    'teachers' => [
+        [
+            'name' => 'Michele',
+            'lastname' => 'Papagni'
+        ],
+        [
+            'name' => 'Fabio',
+            'lastname' => 'Forghieri'
+        ]
+    ],
+    'pm' => [
+        [
+            'name' => 'Roberto',
+            'lastname' => 'Marazzini'
+        ],
+        [
+            'name' => 'Federico',
+            'lastname' => 'Pellegrini'
+        ]
+    ]
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack-6</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+
+<div >
+
+<?php foreach ($db as $key => $value) : ?>
+    <div class="bg-<?= $key === 'teachers' ? 'secondary' : 'success' ?>">
+        <?php foreach ($value as $contents) : ?>
+            <p><?= $contents['name'] . $contents['lastname'] ?> </p>
+        <?php endforeach; ?>
+    </div>
+<?php endforeach; ?>
+
+
+    
+</body>
+</html>
